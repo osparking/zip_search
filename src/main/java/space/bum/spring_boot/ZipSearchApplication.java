f38670fd7bb5a715dc83fff5d3f4ddf9cc20062b
osparking <jbpark03@gmail.com>
2024-03-27 08:12:35 +0900

    프로젝트 생성 초기 상태

---
+++ src/main/java/space/bum/spring_boot/ZipSearchApplication.java
@@ -0,0 +1,13 @@
+package space.bum.spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/space/bum/spring_boot/ZipSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package space.bum.spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
