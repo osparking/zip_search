49ef442b0fce59ddad1d1fe26b8806bb3753a004
osparking <jbpark03@gmail.com>
2024-03-27 20:13:05 +0900

    모델에 검색키 객체 추가

--- src/main/java/space/bum/zip_search/controller/SearchController.java
+++ src/main/java/space/bum/zip_search/controller/SearchController.java
@@ -1,12 +1,17 @@
 package space.bum.zip_search.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import space.bum.zip_search.domain.SearchKey;
 
 @Controller
 public class SearchController {
-  @GetMapping("/search")
-  public String addressSearchForm() {
+  @GetMapping("/searchForm")
+  public String addressSearchForm(Model model) {
+    model.addAttribute("searchKey", new SearchKey());
     return "searchform";
   }
+
 }
