59b6aa556cbbb0ab9817b2b6d9780ecdb560d901
osparking <jbpark03@gmail.com>
2024-03-27 20:45:06 +0900

    모의 검색 결과 반환

--- src/main/java/space/bum/zip_search/controller/SearchController.java
+++ src/main/java/space/bum/zip_search/controller/SearchController.java
@@ -1,5 +1,7 @@
 package space.bum.zip_search.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import jakarta.validation.Valid;
+import space.bum.zip_search.domain.FoundAddress;
 import space.bum.zip_search.domain.SearchKey;
 
 @Controller
@@ -21,6 +24,16 @@ public class SearchController {
   public String addressSearchForm(@Valid SearchKey searchKey,
       BindingResult result, Model model) {
     System.out.println("주소 검색어: " + searchKey.getAddrKey());
+    var addresses = new ArrayList<FoundAddress>();    
+    
+    createDummyAddresses(addresses);
+    model.addAttribute("addresses", addresses);
+    System.out.println("검색 건수: " + addresses.size());    
     return "searchform";
+  }
+  
+  private void createDummyAddresses(ArrayList<FoundAddress> addresses) {
+    addresses.add(new FoundAddress("12345", "서울시 종로구 관철동 134번지"));
+    addresses.add(new FoundAddress("23233", "경기도 하남시 망월동 1050번지"));
   }
 }
