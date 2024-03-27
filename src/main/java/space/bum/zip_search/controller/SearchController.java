549aca290f8904cd50d88d549537bc8df6ce7dee
osparking <jbpark03@gmail.com>
2024-03-27 20:25:19 +0900

    제출된 주소검색 요청 처리기

--- src/main/java/space/bum/zip_search/controller/SearchController.java
+++ src/main/java/space/bum/zip_search/controller/SearchController.java
@@ -2,8 +2,10 @@ package space.bum.zip_search.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import jakarta.validation.Valid;
 import space.bum.zip_search.domain.SearchKey;
 
 @Controller
@@ -13,5 +15,11 @@ public class SearchController {
     model.addAttribute("searchKey", new SearchKey());
     return "searchform";
   }
-
+  
+  @GetMapping("/searchAddress")
+  public String addressSearchForm(@Valid SearchKey searchKey,
+      BindingResult result, Model model) {
+    System.out.println("주소 검색어: " + searchKey.getAddrKey());
+    return "searchform";
+  }
 }
