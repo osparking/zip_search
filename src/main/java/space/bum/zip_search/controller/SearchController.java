535d9860344b0a14c06a22ad40f5dafa0251ab84
osparking <jbpark03@gmail.com>
2024-03-28 22:20:07 +0900

    주소 검색 요청 처리기 개선 - 페이지 반환

--- src/main/java/space/bum/zip_search/controller/SearchController.java
+++ src/main/java/space/bum/zip_search/controller/SearchController.java
@@ -1,43 +1,59 @@
 package space.bum.zip_search.controller;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.validation.Valid;
 import space.bum.zip_search.domain.FoundAddress;
 import space.bum.zip_search.domain.SearchKey;
+import space.bum.zip_search.service.AddressService;
 
 @Controller
 public class SearchController {
-  @GetMapping("/searchForm")
+  @GetMapping("/searchform")
   public String addressSearchForm(Model model) {
     model.addAttribute("searchKey", new SearchKey());
     return "searchform";
   }
 
-  @PostMapping("/searchAddress")
+  @Autowired
+  private AddressService addressService;
+  
+  @GetMapping("/searchAddress")
   public String addressSearchForm(@Valid SearchKey searchKey,
-      BindingResult result, Model model) {
-    System.out.println("주소 검색어: " + searchKey.getAddrKey());
-    var addresses = new ArrayList<FoundAddress>();
-    createDummyAddresses(addresses);
-    model.addAttribute("addresses", addresses);
-    System.out.println("검색 건수: " + addresses.size());
-    return "searchform";
-  }
-
-  private void createDummyAddresses(List<FoundAddress> addresses) {
-    for (int i = 0; i < 500; i++) {
-      addresses.add(new FoundAddress(Integer.toString(12300 + i),
-          "서울시 종로구 관철동 134번지"));
-      addresses.add(new FoundAddress(Integer.toString(57000 + i),
-          "경기도 하남시 망월동 1050번지"));
+      BindingResult result, Model model,
+      @RequestParam("page") Optional<Integer> page,
+      @RequestParam("size") Optional<Integer> size) {
+    int currentPage = page.orElse(1);
+    int pageSize = size.orElse(5);
+    Pageable pageable = PageRequest.of(currentPage - 1, pageSize);
+    Page<FoundAddress> addressPage = addressService.findPaginated(pageable);
+    long total = addressPage.getTotalElements();
+    model.addAttribute("addressPage", addressPage);
+    model.addAttribute("currentPage", addressPage.getNumber() + 1);
+    
+    int totalPages = addressPage.getTotalPages();
+    
+    model.addAttribute("totalPages", totalPages);
+    if (totalPages > 0) {
+      List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+          .boxed()
+          .collect(Collectors.toList());
+      model.addAttribute("pageNumbers", pageNumbers);
     }
+    return "searchform";
   }
 }
