ce811d8c700a45a26f2043d8689a9ac2a5928fc6
osparking <jbpark03@gmail.com>
2024-03-29 15:19:36 +0900

    한 주소 항목은 3 멤버를 가진다

--- src/main/java/space/bum/zip_search/domain/FoundAddress.java
+++ src/main/java/space/bum/zip_search/domain/FoundAddress.java
@@ -2,10 +2,19 @@ package space.bum.zip_search.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.ToString;
 
 @Data
 @AllArgsConstructor
+@ToString
 public class FoundAddress {
   private String zipcode;
-  private String address;
+  /**
+   * 도로명 주소
+   */
+  private String roadAddress;
+  /**
+   * 지번 주소
+   */
+  private String zBunAddress;
 }
